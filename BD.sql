2ca13e1101484afe6d9184be7d5e3ef55f5bee0a
santi <santisamaya@gmail.com>
2022-07-26 16:15:44 -0400

    BD

---
+++ BD.sql
@@ -0,0 +1,54 @@
+user
+    id
+	rut
+	name
+	lastname
+	email
+	phone
+	addres
+	role
+
+product
+    id
+	sku
+	title
+	description
+	price
+	cant
+	state
+	size
+	color
+	category
+
+boleta
+    id
+	nro_boleta
+	id_user            -direccion -nombre-bla
+	products[aray]
+	state
+	tipo_pago
+
+category
+    id
+	title
+
+size
+    id
+	title
+
+color
+    id
+	title
+
+
+role
+    id
+	title
+
+city
+    id
+	title
+
+subsciption
+    id
+	title
